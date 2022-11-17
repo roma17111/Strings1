879e9cfb75ca1269d6a9dc0ab9913d04c62a1b32
Роман <romanze1706@gmail.com>
2022-11-17 15:31:01 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        firstName = firstName + " ";
+        lastName = lastName + " ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника —  " + fullName);
+
 
     }
 }
