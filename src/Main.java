9ef316c2cf9f90c679365e5defaebcc42ba18ea5
Роман <romanze1706@gmail.com>
2022-11-17 16:05:27 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,21 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
 
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
         firstName = firstName + " ";
         lastName = lastName + " ";
         String fullName = lastName + firstName + middleName;
-        firstName = firstName.replace("Ivan", "IVAN");
-        System.out.println("ФИО сотрудника —  " + fullName);
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника —  " + fullName);
 
         fullName = fullName.toUpperCase();
         System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName);
+
+
+
+
 
 
 
